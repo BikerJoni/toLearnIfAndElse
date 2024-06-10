153104f3699a5fc64e9875be61bc99481d281f34
JahongirAbdusamatov <jahongir_7440266@mail.ru>
2024-06-10 10:19:23 +0500

    ArmyAge with IF or else

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// askar yoshini kiritib uni armiyaga bora oladimi yoki yoqmi aniqlaymiz
+// 1. 17 < bolsa, bora olmaysiz.
+// 2. 28 < bo'lsa bora olmaysiz. 
+// 3. 17 yoki 28  bolsa balki bora olasiz.
+// 4. boshqa barcha holatlarda bora olasiz.
+
+using System;
+
+int Army;
+Console.Write("Enter Your age :");
+string inputAge = Console.ReadLine();
+Army = Convert.ToInt32(inputAge);
+bool isMiner = Army < 17;
+bool isTooOld = Army > 28;
+bool isPossible = Army is 17 or 28;
+
+if(isMiner)
+{
+    Console.WriteLine("Bora olmaysiz!");
+}
+else if(isTooOld)
+{
+    Console.WriteLine("Bora olmaysiz!");
+} 
+else if(isPossible)
+{
+    Console.WriteLine("Balki borarsiz");
+}
+else
+{
+    Console.WriteLine("Armiyaga borishingiz mumkin");
+}
